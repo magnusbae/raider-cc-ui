63be66ef8b0f7190b4eaa6d489fa5d760a1453b9
Magnus Bae <magnusbae@gmail.com>
2016-02-20 11:50:58 +0100

    Fixed: not fetching all the data in the world anymore

--- src/core/fetch/fetch.firebase.js
+++ src/core/fetch/fetch.firebase.js
@@ -1,25 +1,26 @@
-import Firebase from 'firebase';   
+import Firebase from 'firebase';
 import FirebaseAction from '../../actions/FirebaseActions.js'
 
 var FirebaseFetcher = {
  init: function(){
   var firebaseRef = new Firebase("https://ariot2016.firebaseio.com/");
-  firebaseRef.child("temperatures").on("child_added", function(dataSnapshot) {
+  firebaseRef.child("temperatures").limitToLast(1).on("child_added", function(dataSnapshot) {
     FirebaseAction.firebaseData(dataSnapshot.val());
   });
-  firebaseRef.child("pictures").on("child_added", function(dataSnapshot) {
+  firebaseRef.child("pictures").limitToLast(1).on("child_added", function(dataSnapshot) {
     FirebaseAction.receivedCameraData(dataSnapshot.val());
   });
-  firebaseRef.child("humidity").on("child_added", function(dataSnapshot) {
+  firebaseRef.child("humidity").limitToLast(1).on("child_added", function(dataSnapshot) {
     FirebaseAction.receivedHumidityData(dataSnapshot.val());
   });
-  firebaseRef.child("barometricpressure").on("child_added", function(dataSnapshot) {
+  firebaseRef.child("barometricpressure").limitToLast(1).on("child_added", function(dataSnapshot) {
     FirebaseAction.receivedBarometerData(dataSnapshot.val());
   });
-  firebaseRef.child("luxometer").on("child_added", function(dataSnapshot) {
+  firebaseRef.child("luxometer").limitToLast(1).on("child_added", function(dataSnapshot) {
     FirebaseAction.receivedAmbientLightData(dataSnapshot.val());
   });
 
   }
-} 
+};
+
 export default FirebaseFetcher;
